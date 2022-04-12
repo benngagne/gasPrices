31606c09d285fbf57c3e5b9a994d1e2ecf46b25c
Benjamin Gagne <benanater01@gmail.com>
2022-04-12 00:43:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# gasprices
+
+API that scrapes [windsorite.ca/gas](https://windsorite.ca/gas) for the top 10 "lowest" gas prices in Windsor, ON, averages the data, then serves both the data and average to the user.
+
+## Usage
+
+Simply run `npm start` to start the node.js server. Change the port at the top of the *server.js* file to your liking.
---
+++ server.js
@@ -0,0 +1,54 @@
+// declare variables
+const express = require('express')
+const app = express()
+const puppeteer = require('puppeteer')
+const port = 8081
+
+// main web scrapper function
+async function webScraper(url) {
+    // boring puppeteer stuff
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(url) // go to url (windsorite.ca/gas)
+    
+    let stores = [] // empty array of stores
+
+    // interate through windsorite gas table and store in object with station name and current price
+    for(let i = 0; i < 10; i++) {
+        const store = {}
+
+        const name = await page.$eval(`#spnGB27562StationNm${i}`, element => element.textContent)
+        const price = await page.$eval(`#spnGB27562Price${i}`, element => element.textContent)
+
+        store.name = name
+        store.price = parseFloat(price)
+
+        stores.push(store)
+    }
+    browser.close() // close puppeteer browser
+
+    // average prices of the gas stations
+    function averageData(stores) {
+        let average = 0
+        stores.forEach((store, idx) => {
+            average += store.price
+        })
+        average /= 10
+        return Math.round((average + Number.EPSILON) * 10) / 10 // return average rounded to 1 decimal point
+    }
+
+    // return stores array and average of the stores in array
+    return [stores, averageData(stores)]
+}
+// main api endpoint
+app.get('/', async (req,res) => {
+    // run web scrapping function, store returned data (asynchronously)
+    const results = await webScraper('https://windsorite.ca/gas')
+    // serve data to user, json format
+    res.json({
+        "stores": results[0],
+        "average": results[1]
+    })
+})
+
+app.listen(port, () => console.log(`listening on port: ${port}`)) // listen on specified port
